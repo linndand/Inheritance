d2f4cb01067f05e65f2af683db38bdd2eed29cf7
linndand <linnda.p.n@students.amikom.ac.id>
2023-06-02 12:24:31 +0700

    Inheritance

---
+++ Inheritance/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        //field
+        string name;
+        int age;
+
+        //constructor
+        public Person(string n, int a)
+        {
+            this.name = n;
+            this.age = a;
+        }
+
+        //properties
+        public string Name;
+
+        public int Age;
+
+        //fungsi
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0}, dan umur saya {1} tahun", name, age);
+        }
+    }
+
+    //class turunan
+    public class Karyawan : Person 
+    {
+        string Subject;
+        string karyawanid;
+
+        //constructor
+        public Karyawan(string kid, string sbj, string n, int a) : base(n, a)
+        {
+            karyawanid = kid;
+            Subject = sbj;
+        }
+
+        //properties
+        public string karyawanID;
+
+        public string subject;
+
+
+        //override
+        public void InfoKaryawan(string kid, string sbj)
+        {
+            //fungsi
+            Console.WriteLine("Dengan ID {0}, \nSaya disini sebagai {1} ", kid, sbj);
+        }
+        public void InfoPerson(string name, int age)
+        {
+            Console.WriteLine("Nama saya {0}, dan umur saya {1} tahun", name, age);
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Linnda Pawidya Nur'aini", 18);
+            person1.InfoPerson();
+
+            Console.WriteLine("---------------------------------");
+
+            Karyawan karyawan1 = new Karyawan("050704", "Dosen", "Keiji", 24);
+            karyawan1.InfoKaryawan("050704", "Dosen");
+            karyawan1.InfoPerson();
+
+            Console.ReadKey();
+        }
+    }
+}
